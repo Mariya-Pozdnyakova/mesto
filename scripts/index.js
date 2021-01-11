5851edd1ea169aac18bb082c11b7c7bcdc19c352
Mariya-Pozdnyakova <mariya94krylova@yandex.ru>
2021-01-11 22:29:19 +0300

    feat: скорректировать javascript

--- scripts/index.js
+++ scripts/index.js
@@ -3,11 +3,25 @@ let formElement = document.querySelector('.overlay__form');
 let profileOverlay = document.querySelector('.overlay');
 let popupCloseButton = document.querySelector('.overlay__close-button');
 let popupEditButton = document.querySelector('.profile-info__edit-button');
-
-// profileOverlay.classList.toggle('profile-overlay_closed');
+ // Находим поля формы в DOM
+let nameInput = formElement.querySelector('.overlay__item_el_heading');
+let jobInput = formElement.querySelector('.overlay__item_el_subheading');
+// Выберите элементы, куда должны быть вставлены значения полей
+let nameText = document.querySelector('.profile-info__user-name');
+let jobText = document.querySelector('.profile-info__user-status');
 
 function popupToggle() {
   profileOverlay.classList.toggle('overlay_opened');
+}
+
+function popupOpen() {
+  nameInput.value = nameText.textContent;
+  jobInput.value = jobText.textContent;
+  popupToggle()
+}
+
+function popupClose() {
+  popupToggle()
 }
 
 // Обработчик «отправки» формы, хотя пока
@@ -16,27 +30,18 @@ function handleFormSubmit (evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
                         // Так мы можем определить свою логику отправки.
                         // О том, как это делать, расскажем позже.
-
-    // Находим поля формы в DOM
-    let nameInput = formElement.querySelector('.overlay__item_el_heading');
-    let jobInput = formElement.querySelector('.overlay__item_el_subheading');
-
-    // Получите значение полей из свойства value
+       // Получите значение полей из свойства value
     let nameInputValue = nameInput.value;
     let jobInputValue = jobInput.value;
-    // Выберите элементы, куда должны быть вставлены значения полей
-    let nameInputText = document.querySelector('.profile-info__user-name');
-    let jobInputText = document.querySelector('.profile-info__user-status');
+
     // Вставьте новые значения с помощью textContent
-    jobInputText.textContent = jobInputValue;
-    nameInputText.textContent = nameInputValue;
-    jobInput.placeholder = jobInputValue;
-    nameInput.placeholder = nameInputValue;
-    popupToggle();
+    jobText.textContent = jobInputValue;
+    nameText.textContent = nameInputValue;
+    popupClose();
 }
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', handleFormSubmit);
-popupEditButton.addEventListener('click', popupToggle);
-popupCloseButton.addEventListener('click', popupToggle);
+popupEditButton.addEventListener('click', popupOpen);
+popupCloseButton.addEventListener('click', popupClose);
