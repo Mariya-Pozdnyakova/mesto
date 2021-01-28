c7e72e3eb2b21ba48b2fe944a646b37c84ee3119
Mariya-Pozdnyakova <mariya94krylova@yandex.ru>
2021-01-28 12:59:48 +0300

    feat: добавить инициацию 6 карточек при загрузке

--- scripts/index.js
+++ scripts/index.js
@@ -9,6 +9,47 @@ let jobInput = formElement.querySelector('.overlay__item_el_subheading');
 // Выберите элементы, куда должны быть вставлены значения полей
 let nameText = document.querySelector('.profile-info__user-name');
 let jobText = document.querySelector('.profile-info__user-status');
+// Находим html - шаблон для фото карточек
+const cardTemplate = document.querySelector('#card-template').content;
+//находим область контейнер для фотокарточек
+const cardsContainer = document.querySelector('.photo-cards');
+
+//массив с данными по фото карточкам
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+// подгрузка 6 карточек при запуске
+initialCards.forEach((item) => {
+  const cardElement = cardTemplate.querySelector('.photo-card').cloneNode(true);
+  cardElement.querySelector('.photo-card__image').src = item.link;
+  cardElement.querySelector('.photo-card__image').alt = item.name;
+  cardElement.querySelector('.photo-card__title').textContent = item.name;
+  cardsContainer.append(cardElement);
+});
 
 function popupToggle() {
   profileOverlay.classList.toggle('overlay_opened');
@@ -41,3 +82,4 @@ function handleFormSubmit (evt) {
 formElement.addEventListener('submit', handleFormSubmit);
 popupEditButton.addEventListener('click', popupOpen);
 popupCloseButton.addEventListener('click', popupToggle);
+
