8601326e415c465263889ea294d5d0b5ab744c8c
Mariya-Pozdnyakova <mariya94krylova@yandex.ru>
2021-01-30 19:36:13 +0300

    feat: настроить интерактивность для формы с карточками

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,25 @@
-// Находим форму в DOM
-let formElement = document.querySelector('.overlay__form');
-let profileOverlay = document.querySelector('.overlay');
-let popupCloseButton = document.querySelector('.overlay__close-button');
-let popupEditButton = document.querySelector('.profile-info__edit-button');
+// Находим Overlay в DOM
+const profileOverlay = document.querySelector('.overlay');
+const formArea = document.querySelector('.overlay__popup-area');
+// Находим формы в DOM
+const profileForm = formArea.querySelector('#profileForm');
+const сardsForm = formArea.querySelector('#cardsForm');
+const popupForms = Array.from(formArea.querySelectorAll('.overlay__form'));
+// Находим кнопки в DOM
+const popupCloseButton = document.querySelector('.overlay__close-button');
+const popupEditButton = document.querySelector('.profile-info__edit-button');
+const popupAddButton = document.querySelector('.profile__add-button');
+const popupSaveButton = document.querySelector('.overlay__save-button');
+// Находим заголовок формы в DOM
+const popupHeading = document.querySelector('.overlay__heading');
  // Находим поля формы в DOM
-let nameInput = formElement.querySelector('.overlay__item_el_heading');
-let jobInput = formElement.querySelector('.overlay__item_el_subheading');
+const nameInput = profileForm.querySelector('.overlay__item_el_heading');
+const jobInput = profileForm.querySelector('.overlay__item_el_subheading');
+const newCardName = сardsForm.querySelector('.overlay__item_el_name');
+const newCardLink = сardsForm.querySelector('.overlay__item_el_link');
 // Выберите элементы, куда должны быть вставлены значения полей
-let nameText = document.querySelector('.profile-info__user-name');
-let jobText = document.querySelector('.profile-info__user-status');
+const nameText = document.querySelector('.profile-info__user-name');
+const jobText = document.querySelector('.profile-info__user-status');
 // Находим html - шаблон для фото карточек
 const cardTemplate = document.querySelector('#card-template').content;
 //находим область контейнер для фотокарточек
@@ -42,7 +53,7 @@ const initialCards = [
   }
 ];
 
-// подгрузка 6 карточек при запуске
+//подгрузка 6 карточек при запуске
 initialCards.forEach((item) => {
   const cardElement = cardTemplate.querySelector('.photo-card').cloneNode(true);
   cardElement.querySelector('.photo-card__image').src = item.link;
@@ -51,35 +62,65 @@ initialCards.forEach((item) => {
   cardsContainer.append(cardElement);
 });
 
-function popupToggle() {
+//вспомогательные функции
+const popupToggle = () => {
   profileOverlay.classList.toggle('overlay_opened');
 }
 
-function popupOpen() {
+const formToggle = (form) => {
+  form.classList.toggle('overlay__form_active');
+}
+//открытие форм
+const profileOpen = () => {
+  popupToggle();
+  formToggle(profileForm);
   nameInput.value = nameText.textContent;
   jobInput.value = jobText.textContent;
+}
+
+const newCardOpen = () => {
   popupToggle();
+  formToggle(сardsForm);
+}
+//Закрытие popup
+const popupClose = () => {
+  popupToggle();
+  popupForms.forEach((item) => {
+    if (item.classList.contains('overlay__form_active')) {
+      formToggle(item);
+    }
+  });
 }
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
-function handleFormSubmit (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                        // Так мы можем определить свою логику отправки.
-                        // О том, как это делать, расскажем позже.
-       // Получите значение полей из свойства value
+const handleFormSubmit = (evt) => {
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+                      // Так мы можем определить свою логику отправки.
+                      // О том, как это делать, расскажем позже.
+      // Получите значение полей из свойства value
+  if (profileForm.classList.contains('overlay__form_active')) {
     let nameInputValue = nameInput.value;
     let jobInputValue = jobInput.value;
-
     // Вставьте новые значения с помощью textContent
     jobText.textContent = jobInputValue;
     nameText.textContent = nameInputValue;
-    popupToggle();
+    formToggle(profileForm);
+  }
+  if (cardsForm.classList.contains('overlay__form_active')) {
+    const cardElement = cardTemplate.querySelector('.photo-card').cloneNode(true);
+    cardElement.querySelector('.photo-card__image').src = newCardLink.value;
+    cardElement.querySelector('.photo-card__image').alt = newCardName.value;
+    cardElement.querySelector('.photo-card__title').textContent = newCardName.value;
+    cardsContainer.prepend(cardElement);
+    formToggle(cardsForm);
+  }
+  popupToggle();
 }
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', handleFormSubmit);
-popupEditButton.addEventListener('click', popupOpen);
-popupCloseButton.addEventListener('click', popupToggle);
-
+  popupEditButton.addEventListener('click', profileOpen);
+  popupAddButton.addEventListener('click', newCardOpen);
+  popupCloseButton.addEventListener('click', popupClose);
+  popupForms.forEach((item) => {item.addEventListener('submit', handleFormSubmit)});
